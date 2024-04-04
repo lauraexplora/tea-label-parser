13b69a0f23366b8d676fde5d11033d12cf2f8d6a
lauraexplora <74592807+lauraexplora@users.noreply.github.com>
2024-04-04 08:21:30 +0200

    example tea label parsing

---
+++ tea_label.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import pytesseract
+from pytesseract import Output
+import re
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+def read_label():
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    t = pytesseract.image_to_string(thresholding(get_grayscale(frame)), lang='deu', output_type=Output.DICT)
+    
+    s = re.search(r"[0-9]+(\s*-\s*)*[0-9]*([,.]+[0-9]+)*(?=\sMin)", t['text'])
+    if (s is not None):
+        return s.group()
+    
+    return s
+
+def label_to_ms(label):
+    # Remove whitespace
+    l = label.replace(' ', '')
+    # Use the minimum brewing time
+    if ('-' in l):
+        l = l.split('-')[0]
+    # Ensure decimals are in non-EU format
+    l.replace(',','.')
+
+    m = float(l)
+    return int(m * 60000)
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+while True:
+    label = read_label()
+    if label:
+        print('MATCH')
+        print(label_to_ms(label))
+        break
+    
+ 
+# When everything done, release the capture
+cap.release()

6089405bb1ec29993113cf5e96398c82184b18c5
lauraexplora <74592807+lauraexplora@users.noreply.github.com>
2024-04-06 19:12:59 +0100

    import rainbowhat examples

---
+++ examples/blinkt/random_blink.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import random
+import time
+
+# import blinkt
+from rainbowhat import rainbow as blinkt
+
+blinkt.set_clear_on_exit()
+
+while True:
+    pixels = random.sample(range(blinkt.NUM_PIXELS), random.randint(1, 5))
+    for i in range(blinkt.NUM_PIXELS):
+        if i in pixels:
+            blinkt.set_pixel(i, 255, 150, 0)
+        else:
+            blinkt.set_pixel(i, 0, 0, 0)
+    blinkt.show()
+    time.sleep(0.05)
